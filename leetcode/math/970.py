e68a24d10dc9f2af7185b1dcd5933935b5254d9f
junyeong-nero <magicwho@unist.ac.kr>
2026-03-10 15:04:58 +0900

    [solve] Powerful Integers
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-opencode)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

---
+++ leetcode/math/970.py
@@ -0,0 +1,19 @@
+class Solution:
+    def powerfulIntegers(self, x: int, y: int, bound: int) -> List[int]:
+        x_arr = [1]
+        y_arr = [1]
+
+        while x > 1 and x_arr[-1] < bound:
+            x_arr.append(x_arr[-1] * x)
+        
+        while y > 1 and y_arr[-1] < bound:
+            y_arr.append(y_arr[-1] * y)
+
+        res = set()
+        for x in x_arr:
+            for y in y_arr:
+                if x + y > bound:
+                    continue
+                res.add(x + y)
+
+        return sorted(res)
