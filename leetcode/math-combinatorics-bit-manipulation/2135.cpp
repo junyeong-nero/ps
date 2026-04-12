e0410d643fa2863c2618df30ac129056d5f93f9f
junyeong-nero <magicwho@unist.ac.kr>
2026-04-12 20:47:59 +0900

    [solve] Count Words Obtained After Adding a Letter

--- leetcode/math-combinatorics-bit-manipulation/2135.cpp
+++ leetcode/math-combinatorics-bit-manipulation/2135.cpp
@@ -1,27 +1,35 @@
 class Solution {
 public:
-    int s2i(string& s) {
-        int ret = 0;
-        for (int i = 0; i < s.length(); i++)
-            ret |= (1 << (s[i] - 'a'));
-        return ret;
+
+    int s2i(string s) {
+        int res = 0;
+        for (char c : s) {
+            res |= (1 << (c - 'a'));
+        }
+        return res;
     }
 
-    int wordCount(vector<string>& sw, vector<string>& tw) {
-        set<int> st;
-        int ans = 0;
-        for (auto& s : sw)
-            st.insert(s2i(s));
-        for (auto& s : tw) {
-            int n = s2i(s);
-            for (int i = 0; i < 26; i++)
-                if (n & (1 << i)) {
-                    if (st.find(n ^ (1 << i)) != st.end()) {
-                        ans++;
-                        break;
-                    }
-                }
+    int wordCount(vector<string>& startWords, vector<string>& targetWords) {
+        
+        map<int,set<int>> d;
+
+        for (string src : startWords) {
+            d[src.size()].insert(s2i(src));
         }
-        return ans;
+
+        int res = 0;
+        for (string target : targetWords) {
+
+            int target_num = s2i(target);
+            for (char c : target) {
+                int temp = target_num ^ (1 << (c - 'a'));
+                if (d[target.size() - 1].count(temp)) {
+                    res++;
+                    break;
+                }
+            }
+        }
+
+        return res; 
     }
 };
